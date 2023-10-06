b8383e63e397b206d9bd586c6b5e743705e9b5f1
Samikshaa00 <147122432+Samikshaa00@users.noreply.github.com>
2023-10-06 12:27:29 +0530

    Add files via upload

---
+++ pract7a.py
@@ -0,0 +1,10 @@
+class student:
+    def__init__(self,name,age): 
+        self.age=name
+        self.age=age
+        s1= student("ash ",15)
+        print(s1.name)
+        print(s1.age)
+        s2=student("pratiksa",14)
+        print(s2.name)
+        print(s2.age)
